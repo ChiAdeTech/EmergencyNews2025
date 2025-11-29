9f6cfce7a69c2f128e9bd2e1eace2713f07756d9
ChiAde Tech <chiadetech@gmail.com>
2025-11-29 22:46:23 +0100

    Gambia and Niger added

--- backend/manual_tasks.py
+++ backend/manual_tasks.py
@@ -1,10 +1,55 @@
-from newsfeeds.tasks import *
+# run_fetch_tasks.py
+
+import os
+import django
+
+# Set up Django environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emergency_news.settings")
+django.setup()
+
+from newsfeeds.tasks import (
+    fetch_bbc_news,
+    fetch_cnn_news,
+    fetch_vanguard_news,
+    fetch_aljazeera_news,
+    fetch_guardian_news,
+    fetch_channelstv_news,
+    fetch_premiumtimes_news,
+    fetch_allafrica_news,
+    fetch_modernghana_news,
+    fetch_myjoyonline_news,
+    fetch_ghheadlines_news,
+    fetch_standard_news,
+    fetch_thepoint_news,
+    fetch_fatunetwork_news,
+    fetch_nigerexpress_news,
+    fetch_lesahel_news,
+    fetch_airinfoagadez_news
+)
+
 
 def run_all_fetch_tasks():
-    # Dynamically collect all fetch functions from globals()
-    tasks_list = [v for k, v in globals().items() if callable(v) and k.startswith("fetch_")]
+    tasks = [
+        fetch_bbc_news,
+        fetch_cnn_news,
+        fetch_vanguard_news,
+        fetch_aljazeera_news,
+        fetch_guardian_news,
+        fetch_channelstv_news,
+        fetch_premiumtimes_news,
+        fetch_allafrica_news,
+        fetch_modernghana_news,
+        fetch_myjoyonline_news,
+        fetch_ghheadlines_news,
+        fetch_standard_news,
+        fetch_thepoint_news,
+        fetch_fatunetwork_news,
+        fetch_nigerexpress_news,
+        fetch_lesahel_news,
+        fetch_airinfoagadez_news
+    ]
 
-    for task in tasks_list:
+    for task in tasks:
         task.delay()
         print(f"Task {task.__name__} triggered successfully.")
 
